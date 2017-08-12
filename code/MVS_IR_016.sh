e2dc092cd068b4d0cd0ba293a5aef84c6db04e50
anonymusICCV <anonymusiccv@gmail.com>
2017-08-12 10:29:00 +0200

    Update README.md

--- code/MVS_IR_016.sh
+++ code/MVS_IR_016.sh
@@ -4,4 +4,4 @@ set -e
 export PATH="/home/user/conda/bin:$PATH"
 
 ./build/tools/caffe train \
-  --solver=examples/mvsir/solver-MVS_IR_016.prototxt $@
+  --solver=examples/code/solver-MVS_IR_016.prototxt $@
